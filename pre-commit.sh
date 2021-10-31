39f488085f7891eaaf7f330b2659e7a6867e3254
manasm11 <53897110+manasm11@users.noreply.github.com>
2021-10-31 21:00:43 +0530

    Corrected algo to find file size

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,7 +3,7 @@ MAX_FILE_SIZE_IN_MB=1
 
 git status
 exec < /dev/tty
-read -p "Are you sure you want to commit these changes ?(y/N)" CONFIRM
+read -p "Are you sure you want to commit these changes ?(y/N): " CONFIRM
 CONFIRM=$(echo $CONFIRM | tr a-z A-Z | xargs )
 [ "$CONFIRM" != "Y" ] && echo "Aborting the commit" && exit 2
 
@@ -11,7 +11,7 @@ ADDED_FILES=$(git status -s | grep "^[AM]" | cut -c4-)
 
 for FILE in $ADDED_FILES;do
     #echo checking file size of $FILE
-    FILE_SIZE_IN_MB=$(du -m $FILE|cut -f1)
+    FILE_SIZE_IN_MB=$(echo $(wc -c $FILE | xargs | cut -d" " -f1)/1000000 | bc)
     if [ $FILE_SIZE_IN_MB -ge $MAX_FILE_SIZE_IN_MB ];then
         echo
         echo "[ERROR]: File size too large, cannot commit ${FILE} (${FILE_SIZE_IN_MB} MB)"
