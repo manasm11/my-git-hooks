6b2b45f349606ec8d70fe270c563466f9f6947b9
Manas Mishra <mmanas@vmware.com>
2021-10-31 11:25:30 +0530

    Added pre-commit hook to check file size

---
+++ pre-commit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git status
+exec < /dev/tty
+read -p "Are you sure you want to commit these changes ?(y/N)" CONFIRM
+CONFIRM=$(echo $CONFIRM | tr a-z A-Z | xargs )
+[ "$CONFIRM" != "Y" ] && echo "Aborting the commit" && exit 2
+
+MAX_FILE_SIZE_IN_MB=1
+ADDED_FILES=$(git status -s | grep "^[AM]" | cut -c4-)
+
+for FILE in $ADDED_FILES;do
+    #echo checking file size of $FILE
+    FILE_SIZE_IN_MB=$(du -m $FILE|cut -f1)
+    if [ $FILE_SIZE_IN_MB -ge $MAX_FILE_SIZE_IN_MB ];then
+        echo
+        echo "[ERROR]: File size too large, cannot commit ${FILE} (${FILE_SIZE_IN_MB} MB)"
+        exit 1
+    fi
+done
